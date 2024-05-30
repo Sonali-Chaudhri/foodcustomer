c85e7a5c5b13c966e7649276f22ef271fa3d9b87
SonaliChaudhari <160842768+Sonali-Chaudhri@users.noreply.github.com>
2024-05-30 13:36:03 +0530

    done

--- src/componants/About.js
+++ src/componants/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import {  Col, Row } from "react-bootstrap";
 import "../AllCss/Aboutus.css";
 const About = () => {
   return (
